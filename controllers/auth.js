85c569fc0bbae59c271c529fbb95bc6018394d72
Oluwatobi Adegelu <adeniyi.adegelu@gmail.com>
2020-07-02 15:58:11 +0100

    Login route create. A user can login with registered email and password

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const User = require("../models/User");
 
 
 //"description      Register User
-//route         /api/v1/auth/register
+//route         POST - /api/v1/auth/register
 //@access        public
 exports.register = asyncHandler(async(req, res, next) => {
     //Create User 
@@ -16,6 +16,39 @@ exports.register = asyncHandler(async(req, res, next) => {
         password,
         role
     })
+    //Create Token
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({success: true, token})
+})
+
+//Login User
+//"description      Register User
+//route        POST - /api/v1/auth/login
+//@access        public
+exports.login = asyncHandler(async(req, res, next) => {
+    //Create User 
+    const { email, password} = req.body;
+
+    //Validate email and password
+    if(!email || !password){
+        return next(new ErrorResponse('Please provide an email and password', 400))
+    }
+    //check user
+    const user = await User.findOne({email}).select('+password');
+
+    //make sure user exist
+    if(!user){
+        return next(new ErrorResponse('invalid credentials', 401))
+    }
+
+    //check if password matches
+    const isMatch = await user.matchPassword(password)
+
+    if (!isMatch){
+        return next(new ErrorResponse('invalid credentials', 401))
+    }
+
     //Create Token
     const token = user.getSignedJwtToken();
 
